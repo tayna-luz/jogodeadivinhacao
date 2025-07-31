c3625febce0b98ab7dd43083e3daaae90e41f665
tayna-luz <tayna.luz.santos@escola.pr.gov.br>
2025-07-31 14:49:58 +0000

    parter 2 do jogo

--- jogo.py
+++ jogo.py
@@ -5,10 +5,22 @@ print("#              ❤️‍🩹Loirinha❤️‍🩹                        
 print("##################################################################")
 print("                     ❤️‍🩹                                           ")
 
-numeroSecreto= random.randragem(0,100)
+numeroSecreto= random.randrange (0,100)
 totalTentativas = 0
 pontos = 100
 
+print("qual niveis de dificuldade?")
+print("(1)- fácil (2)- médio (3)- difícil ")
 
+nivel = int(input("Escolha um nível "))
 
+if(nivel == 1):
+    print(" 20 tentativas")
+if(nivel == 2 ):
+    print(" 10 tentativas")
+    totalTentativas = 10 
+elif(nivel == 3):
+    print("5 tentativas")
+    totalTentativas = 5
+    
 
