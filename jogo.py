b48c0a3960a056ba80a6aea17185caaa4d0fe013
tayna-luz <tayna.luz.santos@escola.pr.gov.br>
2025-07-31 12:17:49 +0000

    primeira parte do jogo

---
+++ jogo.py
@@ -0,0 +1,14 @@
+import random
+print("##################################################################")
+print("#       Jogo de adivinhação                                     # ")
+print("#              ❤️‍🩹Loirinha❤️‍🩹                                     # ")
+print("##################################################################")
+print("                     ❤️‍🩹                                           ")
+
+numeroSecreto= random.randragem(0,100)
+totalTentativas = 0
+pontos = 100
+
+
+
+
